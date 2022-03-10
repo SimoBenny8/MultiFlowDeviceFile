1f0d45df6927688588dcabae2eb77866626490bf
Simone Benedetti <simone.benedetti.416346@alumni.uniroma2.eu>
2022-03-10 15:24:22 +0100

    commit lock

--- multi-flow-device.c
+++ multi-flow-device.c
@@ -245,7 +245,7 @@ static ssize_t dev_write(struct file *filp, const char *buff, size_t len, loff_t
       //caso con waitqueue
       printk(KERN_INFO "Case Non Blocking with priority\n");
       ret_mutex = mutex_trylock(&(the_object->hp_operation_synchronizer));//controllare EBUSY
-      if (ret_mutex != 0){
+      if (ret_mutex == EBUSY){
         int ret_wq = wait_event_timeout(the_object -> hp_queue, mutex_trylock(&(the_object->hp_operation_synchronizer)) == 0, (HZ)*the_object -> timeout);
         if(!ret_wq){
           printk("Timeout expired\n");
@@ -370,11 +370,11 @@ static ssize_t dev_read(struct file *filp, char *buff, size_t len, loff_t *off)
           return -ETIMEDOUT;
         }
       }
-      //wait queue da inserire
+      
     }else{
-      mutex_lock_interruptible(&(the_object->lp_operation_synchronizer));
+      ret_mutex = mutex_lock_interruptible(&(the_object->lp_operation_synchronizer));
       if (ret_mutex != 0){
-        //init_waitqueue_entry(&wait, current);
+        
         int ret_wq = wait_event_timeout(the_object -> lp_queue, mutex_lock_interruptible(&(the_object->lp_operation_synchronizer)) == 0, (HZ)*the_object -> timeout);
         
         if(!ret_wq){
